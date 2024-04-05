d158629bdbe1220b9991869cf2da50f01334dc8c
holynekk <e238109@metu.edu.tr>
2024-04-05 19:36:38 +0300

    Execution code blocks have been taken out of main method.

--- main.c
+++ main.c
@@ -9,15 +9,66 @@
 
 #define BUF_SIZE 128
 
+void execute_single_command(single_input * pipeline_inputs) {
+    int status;
+    pid_t pid = fork();
+    if (pid != 0) {
+        waitpid(pid, &status, 0);
+    } else {
+        execvp(pipeline_inputs[0].data.cmd.args[0], pipeline_inputs[0].data.cmd.args);
+        exit(0);
+    }
+}
+
+void execute_pipeline(single_input * pipeline_inputs, int n) {
+    pid_t pid;
+    int ** pipes = (int **) malloc((n-1) * sizeof(int *)), status;
+
+    for (int i = 0; i < n-1; i++) {
+        pipes[i] = (int*) malloc(2 * sizeof(int));
+        pipe(pipes[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        pid = fork();
+        if (pid == 0) {
+            single_input input_val = pipeline_inputs[i];
+            if (i > 0) {
+                dup2(pipes[i-1][0], 0);
+            }
+            if (i < n-1) {
+                dup2(pipes[i][1], 1);
+            }
+            for (int k = 0; k < n-1; k++) {
+                close(pipes[k][0]);
+                close(pipes[k][1]);
+            }
+            execvp(pipeline_inputs[i].data.cmd.args[0], pipeline_inputs[i].data.cmd.args);
+            exit(0);
+        }
+    }
+
+    for (int k = 0; k < n-1; k++) {
+        close(pipes[k][0]);
+        close(pipes[k][1]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        wait(&status);
+    }
+
+    free(pipes);
+}
+
 int main(void) {
     char *line = malloc(BUF_SIZE * sizeof(char));
-    int status;
-    pid_t pid;
 
     parsed_input *p_input = (parsed_input *) malloc(sizeof(parsed_input));
     printf("/> ");
     while (fgets(line, BUF_SIZE, stdin)) {
         parse_line(line, p_input);
+        printf("Seperator: %d\n", p_input->separator);
+        printf("Num of inputs: %d\n", p_input->num_inputs);
         // pretty_print(p_input);
         if (p_input->separator == SEPARATOR_NONE && p_input->num_inputs == 1) {
             if (strcmp(*(p_input->inputs[0].data.cmd.args), "quit") == 0) {
@@ -26,51 +77,15 @@ int main(void) {
                 /*
                 Single command execution.
                 */
-                pid = fork();
-                if (pid != 0) {
-                    waitpid(pid, &status, 0);
-                } else {
-                    execvp(p_input->inputs[0].data.cmd.args[0], p_input->inputs[0].data.cmd.args);
-                    exit(0);
-                }
+                execute_single_command(p_input->inputs);
             }
         } else if (p_input->separator == SEPARATOR_PIPE) {
             /*
             Pipeline execution.
             */
-            int n = p_input->num_inputs;
-            int ** pipes = (int **) malloc((n-1) * sizeof(int *));
-            for (int i = 0; i < n-1; i++) {
-                pipes[i] = (int*) malloc(2 * sizeof(int));
-                pipe(pipes[i]);
-            }
-            for (int i = 0; i < n; i++) {
-                pid = fork();
-                if (pid == 0) {
-                    single_input input_val = p_input->inputs[i];
-                    if (i > 0) {
-                        dup2(pipes[i-1][0], 0);
-                    }
-                    if (i < n-1) {
-                        dup2(pipes[i][1], 1);
-                    }
-                    for (int k = 0; k < n-1; k++) {
-                        close(pipes[k][0]);
-                        close(pipes[k][1]);
-                    }
-                    execvp(p_input->inputs[i].data.cmd.args[0], p_input->inputs[i].data.cmd.args);
-                    exit(0);
-                }
-            }
-            pid_t pidddd;
-            for (int k = 0; k < n-1; k++) {
-                close(pipes[k][0]);
-                close(pipes[k][1]);
-            }
-            for (int i = 0; i < n; i++) {
-                pidddd = wait(&status);
-            }
-            free(pipes);
+            execute_pipeline(p_input->inputs, p_input->num_inputs);
+        } else if (p_input->separator == SEPARATOR_SEQ) {
+            printf("Hello!\n");
         }
         printf("/> ");
     }
@@ -82,6 +97,7 @@ int main(void) {
 
 // ls -l | tr /a-z/ /A-Z/
 // cat input.txt | grep "log" | wc -c
+// ls -l ; ls -al /dev
 // ls -l | tr /a-z/ /A-Z/ ; echo "Done."
 // cat input.txt | grep "log" | wc -c ; ls -al /dev
 // ls -l | tr /a-z/ /A-Z/ , echo "Done."
